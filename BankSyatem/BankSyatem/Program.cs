d5a3452f66bf175b10cc979c7f3c9e9e5bdbd650
sshekhas <55695640+sshekhas@users.noreply.github.com>
2019-10-18 09:16:18 +0530

    Add files via upload

---
+++ BankSyatem/BankSyatem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSyatem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean status = true;
+            int option,acc;
+            Console.WriteLine("welcome to bank system");
+            Console.WriteLine("you need to enter name and account opening balance to create account");
+            UserDetails[] userdetails = new UserDetails[10];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("{0} name and account opening balance",(i+1));
+                userdetails[i]= new UserDetails(Console.ReadLine(), Convert.ToInt64(Console.ReadLine()),(i+1));
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                userdetails[i].DisplayUserDetails();
+            }
+
+            while (status)
+            {
+                Console.WriteLine("select one of the options\n0.Exit\n1.Deposit\n2.Withdraw");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0: status = false;
+                        break;
+                    case 1: Console.WriteLine("enter the account no. to deposit in");
+                        acc = int.Parse(Console.ReadLine());
+                        Console.WriteLine("enter amount to deposit");
+                        userdetails[(acc-1)].DepositToAcc(Convert.ToInt64(Console.ReadLine()));
+                        break;
+                    case 2:
+                        Console.WriteLine("enter the account no. to withdraw from");
+                        acc = int.Parse(Console.ReadLine());
+                        Console.WriteLine("enter amount you want to withdraw");
+                        userdetails[(acc-1)].WithDrawFromAcc(Convert.ToInt64(Console.ReadLine()));
+                        break;
+                    default: Console.WriteLine("enter valid option");
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ BankSyatem/BankSyatem/UserDetails.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSyatem
+{
+    class UserDetails
+    {
+        private string name;
+        private int accNo;
+        private long balance;
+        //internal int count = 0;
+
+        public UserDetails(string name, long balance,int accNo)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.accNo = accNo;
+            Console.WriteLine("account created");
+        }
+        public void DisplayUserDetails()
+        {
+            Console.WriteLine("NAME: {0}\nACCOUNT NUMBER: {1}\nBALANCE: {2}",name,accNo,balance);
+        }
+        public void DepositToAcc(long deposit)
+        {
+            balance = (balance + deposit);
+            Console.WriteLine("Deposited {0} to account with name {1}\nFinal balance is {2}",deposit,name,balance);
+
+        }
+        public void WithDrawFromAcc(long withdraw)
+        { if ((balance - withdraw) < 500)
+            {
+                Console.WriteLine("can not withdraw {0} ERROR voilate MIN balance",withdraw);
+            }
+            else 
+            {
+                balance = (balance - withdraw);
+                Console.WriteLine("Withdrawn {0} from account with name {1}\nFinal balance is {2}",withdraw,name,balance);
+            }
+            
+        }
+
+    }
+}
